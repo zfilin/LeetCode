270ece8bca5e9e421881fa898366e1a8dd860650
Green FiLin <me@zfilin.org.ua>
2022-03-15 14:33:18 +0200

    https://leetcode.com/problems/two-sum/

--- src/ua/com/unicatstudio/leetcode/p001_TwoSum.java
+++ src/ua/com/unicatstudio/leetcode/p001_TwoSum.java
@@ -2,7 +2,18 @@ package ua.com.unicatstudio.leetcode;
 
 public class p001_TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int result[] = {1,2};
+        int[] result = new int[2];
+        boolean isFound = false;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++)
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    isFound = true;
+                    break;
+                }
+            if (isFound) break;
+        }
         return result;
     }
 }
