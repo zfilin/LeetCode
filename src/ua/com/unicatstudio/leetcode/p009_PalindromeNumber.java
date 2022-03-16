d57df1a01c43aee8cbc0f197c7b4f60d04e090c6
Green FiLin <me@zfilin.org.ua>
2022-03-16 19:33:12 +0200

    9. Palindrome Number

--- src/ua/com/unicatstudio/leetcode/p009_PalindromeNumber.java
+++ src/ua/com/unicatstudio/leetcode/p009_PalindromeNumber.java
@@ -1,8 +1,17 @@
 package ua.com.unicatstudio.leetcode;
 
 public class p009_PalindromeNumber {
+    // https://leetcode.com/problems/palindrome-number/
     public boolean isPalindrome(int x) {
-        // gf
-        return true;
+        int sourceX = x;
+        long multiplier = 1;
+        while (multiplier * 10 < x) multiplier *= 10;
+        int resultX = 0;
+        while(x > 0) {
+            resultX += (x % 10) * multiplier;
+            multiplier /= 10;
+            x /= 10;
+        }
+        return (sourceX == resultX);
     }
 }
--- test/ua/com/unicatstudio/leetcode/p009_PalindromeNumberTest.java
+++ test/ua/com/unicatstudio/leetcode/p009_PalindromeNumberTest.java
@@ -8,7 +8,12 @@ class p009_PalindromeNumberTest {
 
     @Test
     void isPalindrome() {
-        boolean result = new p009_PalindromeNumber().isPalindrome(1234321);
+        boolean result;
+
+        result = new p009_PalindromeNumber().isPalindrome(1410110141);
         assertTrue(result);
+
+        result = new p009_PalindromeNumber().isPalindrome(123456);
+        assertFalse(result);
     }
 }
