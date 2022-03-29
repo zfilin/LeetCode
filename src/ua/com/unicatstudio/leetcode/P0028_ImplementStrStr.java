f3a8c645e4b612654c21dd907a79fa2376af8983
Vladimir <15330763+robodorm@users.noreply.github.com>
2022-03-29 23:03:49 +0200

    fmt )

--- src/ua/com/unicatstudio/leetcode/P0028_ImplementStrStr.java
+++ src/ua/com/unicatstudio/leetcode/P0028_ImplementStrStr.java
@@ -4,20 +4,41 @@ public class P0028_ImplementStrStr {
     // https://leetcode.com/problems/implement-strstr/
 
     public int strStr(String haystack, String needle) {
-        if (needle.isEmpty()) return 0;
+        if (needle.isEmpty()) {
+            return 0;
+        }
+        
         char[] haystackChar = haystack.toCharArray();
         char[] needleChar = needle.toCharArray();
+        
         int step = 1;
-        if (needleChar.length > 2)
-            while(step < needleChar.length-1 && needleChar[0] != needleChar[step]) step++;
+        
+        if (needleChar.length > 2) {
+            while(step < needleChar.length-1 && needleChar[0] != needleChar[step]) {
+                step++;
+            }
+        }
+        
         int ph = 0;
+        
         while (ph <= haystackChar.length - needleChar.length) {
             int pn = 0;
-            while (pn < needleChar.length && haystackChar[ph+pn] == needleChar[pn]) pn++;
-            if (pn == needleChar.length) return ph;
-            if (pn == 0) pn = 1;
+            
+            while (pn < needleChar.length && haystackChar[ph+pn] == needleChar[pn]) {
+                pn++;
+            }
+            
+            if (pn == needleChar.length) {
+                return ph;
+            }
+            
+            if (pn == 0) {
+                pn = 1;
+            }
+            
             ph += Math.min(step, pn);
         }
+        
         return -1;
     }
 }
