a0d4d1b25f5218f226724d29e7db05af90f2b6d7
Green FiLin <me@zfilin.org.ua>
2022-04-08 22:40:20 +0300

    code style

--- src/ua/com/unicatstudio/leetcode/P0035_SearchInsertPosition.java
+++ src/ua/com/unicatstudio/leetcode/P0035_SearchInsertPosition.java
@@ -4,22 +4,30 @@ public class P0035_SearchInsertPosition {
   // https://leetcode.com/problems/search-insert-position/
 
   public int searchInsert(int[] nums, int target) {
-    if (nums.length == 0) return 0;
-    else if (nums[0] > target) return 0;
-    else if (nums[nums.length - 1] < target) return nums.length;
 
-    int wStart = 0;
-    int wEnd = nums.length;
-    while (wEnd - wStart > 1) {
-      int wMid = (wEnd - wStart) / 2 + wStart;
-      if (target < nums[wMid]) {
-        wEnd = wMid;
+    if (nums.length == 0) {
+      return 0;
+    } else if (nums[0] > target) {
+      return 0;
+    } else if (nums[nums.length - 1] < target) {
+      return nums.length;
+    }
+
+    int winStart = 0;
+    int winEnd = nums.length;
+    while (winEnd - winStart > 1) {
+      int winMid = (winEnd - winStart) / 2 + winStart;
+      if (target < nums[winMid]) {
+        winEnd = winMid;
       } else {
-        wStart = wMid;
+        winStart = winMid;
       }
     }
 
-    if (nums[wStart] == target) return wStart;
-    else return wStart + 1;
+    if (nums[winStart] == target) {
+      return winStart;
+    } else {
+      return winStart + 1;
+    }
   }
 }
