baeff1934da0f1c6492e5333ba7eef3f9bb45a07
Green FiLin <me@zfilin.org.ua>
2022-05-03 20:50:53 +0300

    58. Length of Last Word (improvement)

--- src/ua/com/unicatstudio/leetcode/P0058_LengthOfLastWord.java
+++ src/ua/com/unicatstudio/leetcode/P0058_LengthOfLastWord.java
@@ -4,20 +4,7 @@ public class P0058_LengthOfLastWord {
   // https://leetcode.com/problems/length-of-last-word/
 
   public int lengthOfLastWord(String s) {
-    int result = 0;
-    char[] chars = s.toCharArray();
-    boolean isWord = false;
-    for (int i = chars.length - 1; i >= 0; i--) {
-      if (chars[i] != ' ') {
-        isWord = true;
-      }
-      if (isWord) {
-        if (chars[i] == ' ') {
-          return result;
-        }
-        result++;
-      }
-    }
-    return result;
+    s = s.stripTrailing();
+    return s.length() - s.lastIndexOf(' ') - 1;
   }
 }
